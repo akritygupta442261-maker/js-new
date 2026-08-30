5ad533168712682d2d46d20e4edcff57cf8a7e54
Akrity  Gupta <akritygupta442261@gmail.com>
2026-08-30 02:36:53 +0000

    data type learned

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId= 12345
+let accountEmail="akritygupta442261@gmail.com"
+var accountPass= "12345"
+accountcity="jaipur"
+
+let accountdetails
+
+ //accountId=2  not allowed to change const
+ console.log(accountId);
+
+ accountEmail="ak@"
+ accountPass="23"
+accountcity="Bengluru"
+
+ console.log(accountId);
+ console.table([accountId,accountEmail, accountPass, accountcity])
+
+ /* node 01_basics/01_variable.js used to run*/
