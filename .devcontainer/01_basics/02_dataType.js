561c088f28a05eec091922a4428960cb102d455b
Akrity  Gupta <akritygupta442261@gmail.com>
2026-08-30 02:16:01 +0000

    learned datatype

---
+++ .devcontainer/01_basics/02_dataType.js
@@ -0,0 +1,22 @@
+//node 01_basics/02_dataType.js
+
+"use strict" // treat all Js code as newer version
+
+//alert statement
+console.log(3+3);
+console.log("akrity")
+
+
+///------------------------------------
+
+//PRIMITIVE DATA TYPE
+//number=>2 to power 53
+//bigint
+//string=>" "
+//boolean=> true/false
+//null =>standalone value --- ye khali hai 
+//undefined-- abhi value assign  ni hua hai
+//symbol=> uniqueness 
+//object
+
+
