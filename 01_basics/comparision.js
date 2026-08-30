44c585028e931a80ff4e33087a3ac2a918ec1fae
Akrity  Gupta <akritygupta442261@gmail.com>
2026-08-30 18:04:19 +0000

    update basic files

---
+++ 01_basics/comparision.js
@@ -0,0 +1,19 @@
+console.log(2>1)
+
+//alws conform that data type should be same of both element whosse comparision is to do
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+console.log(undefined>=0)
+console.log(undefined<=0)
+console.log(undefined==0)
+
+//=== strict ckeck -it cheks value and data type
+console.log("2"===2)
+console.log("2"==2)
+
+
+
+//-----------[DATA TYPE]----------------------------------
+
